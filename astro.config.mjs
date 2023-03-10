bb0dfbf2e73138c9c982b06d6ddc03b6de9eada3
Frank Zinsli <frankie_96@windowslive.com>
2023-03-10 14:07:30 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://frankzinsli.ch',
+  integrations: [
+    compress({
+      css: true,
+      html: true,
+      img: false,
+      js: true,
+      svg: true
+    }),
+    sitemap(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    })
+  ]
+});
