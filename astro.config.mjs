6b5932809d52774e6c99f60358ac18a7a4486a2e
Frank Zinsli <frankie_96@windowslive.com>
2023-03-10 15:17:45 +0100

    add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,18 @@ import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://frankzinsli.ch',
-  integrations: [
-    compress({
-      css: true,
-      html: true,
-      img: false,
-      js: true,
-      svg: true
-    }),
-    sitemap(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
-    }),
-  ]
+  integrations: [compress({
+    css: true,
+    html: true,
+    img: false,
+    js: true,
+    svg: true
+  }), sitemap(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), vue()]
 });
