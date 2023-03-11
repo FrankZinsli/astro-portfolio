7a52489a9782a1ad953fa948621a0b03439f5714
Frank Zinsli <frankie_96@windowslive.com>
2023-03-11 10:47:30 +0100

    rm manifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@ import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 import vue from "@astrojs/vue";
-import webmanifest from "astro-webmanifest";
-
-import matomo from "astro-matomo";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,26 +15,5 @@ export default defineConfig({
     svg: true
   }), sitemap(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), vue(), webmanifest({
-    name: 'Frankly Web',
-    /**
-     * optional
-     **/
-    icon: 'src/assets/frankly-icon.svg',
-    // source for favicon & icons
-
-    short_name: 'Frankly',
-    description: 'Vom Code zur Kreativität: Folgen Sie mir auf meinem digitalen Weg',
-    start_url: '/',
-    theme_color: '#3367D6',
-    background_color: '#3367D6',
-    display: 'standalone'
-  }), matomo({
-    enabled: import.meta.env.PROD, // Only load in production
-    host: "https://cdn.frankzinsli.ch/matomo/",
-    siteId: 2,
-    heartBeatTimer: 5,
-    disableCookies: true,
-    debug: false,
-  })]
+  }), vue()]
 });
